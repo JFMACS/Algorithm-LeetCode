4bd4e241325eb04ccb5b950bb5f6f763255f07fe
JFMACS <31764113+JFMACS@users.noreply.github.com>
2018-08-09 17:16:19 +0800

    test

---
+++ 198 - House Robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        int count = nums.length;
+        if (count == 0){
+            return 0;
+        }
+        if (count == 1){
+            return nums[0];
+        }
+        int[] max = new int[count];
+	    max[0] = nums[0];
+	    max[1] = (nums[0] > nums[1]) ? nums[0] : nums[1];
+    	for (int i = 2 ; i < count ; i++){
+    	    max[i] = ((max[i - 2] + nums[i]) > max[i - 1]) ? (max[i - 2] + nums[i]) : max[i - 1];
+    	}
+        return max[count-1];
+    }
+}
