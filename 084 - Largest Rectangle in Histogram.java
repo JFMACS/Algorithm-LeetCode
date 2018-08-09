449f03d87a2ac3503ae1d46071251f64a2ffa703
JFMACS <xrf1104157638@gmail.com>
2018-08-09 21:55:08 +0800

    第84题

---
+++ 084 - Largest Rectangle in Histogram.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int count = heights.length;
+        int max = 0;
+        HashSet hs = new HashSet();
+        
+        for (int i = 0; i < count; i++) {
+            if( hs.contains(heights[i]) ) {
+                continue;
+            }
+            hs.add(heights[i]);
+            int height = heights[i];
+            int weight = 0;
+            int maxWeight = 0;
+            for (int j = 0; j < count; j++) {
+                if (heights[j] < height) {
+                    if (weight > maxWeight) {
+                        maxWeight = weight;
+                    }
+                    weight = 0;
+                } else {
+                    weight ++;
+                }
+            }
+            if (weight > maxWeight) {
+                maxWeight = weight;
+            }
+            if (maxWeight * height > max) {
+                max = maxWeight * height;
+            }
+        }
+        return max;
+    }
+}
