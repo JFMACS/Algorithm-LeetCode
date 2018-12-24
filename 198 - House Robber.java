ce1ba44c99df585e818717853f5547ba69dd52b9
JFMACS <31764113+JFMACS@users.noreply.github.com>
2018-12-24 10:42:23 +0800

    Update 198 - House Robber.java

--- 198 - House Robber.java
+++ 198 - House Robber.java
@@ -1,8 +1,8 @@
-/*
-动态规划
-计f(x)为到达第x家时可以获取的最大金额
-则f(x)为f(x-1)与f(x-2)+num(x)的较大值
-*/
+/**
+ * 动态规划
+ * 计f(x)为到达第x家时可以获取的最大金额
+ * 则f(x)为f(x-1)与f(x-2)+num(x)的较大值
+ */
 
 class Solution {
     public int rob(int[] nums) {
