709f9f88c549262b735efab31f7cfb1cf6ed01be
JFMACS <31764113+JFMACS@users.noreply.github.com>
2018-12-24 10:40:35 +0800

    Update 017 - Letter Combinations of a Phone Number.java

--- 017 - Letter Combinations of a Phone Number.java
+++ 017 - Letter Combinations of a Phone Number.java
@@ -1,10 +1,9 @@
-/*
-1.先计算出List中元素个数，初始化字符串数组
-2.从第一位开始批量编辑数组
-  比如共9个字符串，第一位为abc，则0-2为a，3-5为b，6-8为c
-3.将字符串添加到List
-*/
-
+/**
+ * 1.先计算出List中元素个数，初始化字符串数组
+ * 2.从第一位开始批量编辑数组
+ * 比如共9个字符串，第一位为abc，则0-2为a，3-5为b，6-8为c
+ * 3.将字符串添加到List
+ */
 class Solution {
     static char[][] code = {"".toCharArray(), "".toCharArray(), "abc".toCharArray(),
         "def".toCharArray(), "ghi".toCharArray(), "jkl".toCharArray(),
@@ -17,7 +16,7 @@ class Solution {
 		int count = 1;
 		char[] ch = digits.toCharArray();
 		int[] digit = new int[ch.length];
-		//预先计算List的长度
+		// 预先计算List的长度
 		for (int i = 0; i < ch.length; i++) {
 			digit[i] = ch[i] - '0';
 			if (digit[i] != 7 && digit[i] != 9) {
